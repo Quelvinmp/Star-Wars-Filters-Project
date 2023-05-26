64aa0ee4762b2d31c9e7e15d2bcf51fa1c542216
Quelvinmp <quelvinmp@gmail.com>
2023-05-26 13:28:08 -0300

    fix: lint

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from 'react';
 import { PlanetsContext } from '../contexts/PlanetsProvider';
 import Order from './FiltersComponents/Order';
