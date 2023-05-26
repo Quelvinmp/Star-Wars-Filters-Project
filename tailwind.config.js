96b733b5a71b4956f9bc98b47bdb85602d5cc5b0
Quelvinmp <quelvinmp@gmail.com>
2023-05-26 09:42:18 -0300

    wip: adiciona imagens para background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+/* eslint-disable quotes */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: "url('./src/assets/fundo-star-wars.svg')",
+        circles: "url('./src/assets/grafismo-topo.svg')",
+        logo: "url('./src/assets/logo-star-wars.svg')",
+      },
+    },
   },
   plugins: [],
 };
